1f77853a3a3fd0a79e4676bcb7b1f2cb357a0587
Sergey Tkachenko <Tkachenko@tpg.local>
2017-11-17 10:38:01 +0200

    small bug fixes

--- puns/views.py
+++ puns/views.py
@@ -25,30 +25,30 @@ def get_word(request):
 def result_en(request, input_word, comb):
     pattern = r"[1-9]"
     error = False
-    combi_pun = comb != 'y'
+    combi_pun = comb == 'y'
     if re.search( pattern, input_word ): error = True
 
-    words = list(PunWordEn.objects.all()) if combi_pun else list(PunWordRu.objects.all())
+    words = list(PunWordEn.objects.all()) if not combi_pun else list(PunWordRu.objects.all())
     new_words = [word.word for word in words]
-    output = (pun(new_words, input_word.lower(), comb == 'y'))
+    output = (pun(new_words, input_word.lower(), combi_pun))
     form = NameForm()
     context = {'input_word': input_word, 'form': form, 'new_words': new_words,
                'input_word_check': input_word.lower(), 'output': output[:6],'error':error,
-               'comb': comb}
+               'comb': comb,'combi_pun':combi_pun}
     return render(request, 'puns/result.html', context)
 
 
 def result_ru(request, input_word, comb):
     pattern = r"[1-9]"
     error = False
-    combi_pun = comb != 'y'
+    combi_pun = comb == 'y'
     if re.search( pattern, input_word ): error = True
 
-    words = list(PunWordRu.objects.all()) if combi_pun else list(PunWordEn.objects.all())
+    words = list(PunWordRu.objects.all()) if not combi_pun else list(PunWordEn.objects.all())
     new_words = [word.word for word in words]
-    output = (pun(new_words, input_word.lower(),comb == 'y'))
+    output = (pun(new_words, input_word.lower(),combi_pun))
     form = NameForm()
     context = {'input_word': input_word, 'form': form, 'new_words': new_words,
                'input_word_check': input_word.lower(), 'output': output[:6],'error':error,
-               'comb':comb,}
+               'comb':comb,'combi_pun': combi_pun}
     return render(request, 'puns/result.html', context)
