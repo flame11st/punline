c5a7663e4c05d46ad7b577ac998585c0c96f525d
Sergey Tkachenko <Tkachenko@tpg.local>
2017-11-17 16:15:13 +0200

    small bug fixes

--- puns/urls.py
+++ puns/urls.py
@@ -6,5 +6,5 @@ app_name = 'puns'
 urlpatterns = [
     url(r'^$', views.get_word, name='word'),
     url(r'^res_(?P<comb>[a-z])_(?P<input_word>[A-Za-z1-9]+)/$', views.result_en, name='result_en'),
-    url(r'^res_(?P<comb>[a-z])_(?P<input_word>[А-Яа-я1-9]+)/$', views.result_ru, name='result_ru'),
+    url(r'^res_(?P<comb>[a-z])_(?P<input_word>[А-Яа-я1-9ёЁ]+)/$', views.result_ru, name='result_ru'),
 ]
